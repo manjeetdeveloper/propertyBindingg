11b646d5f4ade3c5ec4ca996bc60d30eb1c75d33
Manjeet Singh <98460132+manjeetdeveloper@users.noreply.github.com>
2024-11-27 15:46:28 +0530

    new updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'propertyBindingg';
+  name = "Manjeet Kumar";
+
+
+  imgpath="https://images.pexels.com/photos/29207369/pexels-photo-29207369/free-photo-of-charming-urban-street-with-bicycles-and-blue-doors.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
 }
